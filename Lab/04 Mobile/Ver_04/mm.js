0ce6e07b6e5fbc3064c5e946f862a7d5d57bfb7c
cheoh966 <cheoh966@users.noreply.github.com>
2018-05-13 23:46:41 -0400

    lab
    a

---
+++ Lab/04 Mobile/Ver_04/mm.js
@@ -0,0 +1,17 @@
+$(".swiper-slide").draggable();
+$(".swiper-slide").droppable();
+var takeScreenShot = function() {
+    html2canvas(document.getElementById("container"), {
+        onrendered: function (canvas) {
+            var tempcanvas=document.createElement('canvas');
+            tempcanvas.width=350;
+            tempcanvas.height=350;
+            var context=tempcanvas.getContext('2d');
+            context.drawImage(canvas,112,0,288,200,0,0,350,350);
+            var link=document.createElement("a");
+            link.href=tempcanvas.toDataURL('image/jpg');   //function blocks CORS
+            link.download = 'screenshot.jpg';
+            link.click();
+        }0    });
+}
+
